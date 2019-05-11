b8136adfbf928913e8937a95135a036c8d09ae66
Le Quoc Viet <vietlq85@gmail.com>
2019-05-11 13:34:24 +0100

    implemented urljoin when adding new URLs. TODO: Add test assertion & tox & set of URLs instead of list

--- tests/test_monzo_com.py
+++ tests/test_monzo_com.py
@@ -3,8 +3,7 @@
 """Test sitemap builder against monzo.com"""
 
 from html.parser import HTMLParser
-
-#TODO: Use current location + <A:href> to get fully qualified URLs
+from urllib.parse import urljoin
 
 
 class UrlHtmlParser(HTMLParser):
@@ -13,12 +12,19 @@ class UrlHtmlParser(HTMLParser):
     def __init__(self):
         super().__init__()
         self.temp_urls = []
+        self.url = None
+
+    def parse_html_with_url(self, html_str, url):
+        self.url = url
+        super().feed(html_str)
 
     def handle_starttag(self, tag, attrs):
         if tag.lower() == 'a':
             for attr in attrs:
                 if attr[0] == 'href':
-                    self.temp_urls.append(attr[1])
+                    temp_url = attr[1]
+                    new_url = urljoin(self.url, temp_url)
+                    self.temp_urls.append(new_url)
 
     def handle_endtag(self, tag):
         pass
@@ -35,5 +41,5 @@ if __name__ == '__main__':
     parser = UrlHtmlParser()
     with open('tests/test_monzo_com.html', 'rb') as fh:
         monzo_com_html = fh.read().decode('utf-8')
-    parser.feed(monzo_com_html)
+    parser.parse_html_with_url(monzo_com_html, "https://monzo.com/")
     print(parser.temp_urls)
