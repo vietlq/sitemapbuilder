7e33e5bd37b6db67cff2e4a333836d47c67cf136
Le Quoc Viet <vietlq85@gmail.com>
2019-05-11 13:21:45 +0100

    fixed pylint & flake8 warnings

--- tests/test_monzo_com.py
+++ tests/test_monzo_com.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+"""Test sitemap builder against monzo.com"""
+
 from html.parser import HTMLParser
 
+
 class UrlHtmlParser(HTMLParser):
+    """HTML parser that extracts URLs from <A> tags"""
+
     def __init__(self):
         super().__init__()
         self.temp_urls = []
@@ -13,8 +18,6 @@ class UrlHtmlParser(HTMLParser):
         if tag.lower() == 'a':
             for attr in attrs:
                 if attr[0] == 'href':
-                    #print(attr[1])
-                    #yield attr[1]
                     self.temp_urls.append(attr[1])
 
     def handle_endtag(self, tag):
@@ -23,8 +26,14 @@ class UrlHtmlParser(HTMLParser):
     def handle_data(self, data):
         pass
 
-parser = UrlHtmlParser()
-with open('tests/test_monzo_com.html', 'rb') as fh:
-    monzo_com_html = fh.read().decode('utf-8')
-parser.feed(monzo_com_html)
-print(parser.temp_urls)
+    def error(self, message):
+        raise NotImplementedError(
+            "subclasses of UrlHtmlParser must override error()")
+
+
+if __name__ == '__main__':
+    parser = UrlHtmlParser()
+    with open('tests/test_monzo_com.html', 'rb') as fh:
+        monzo_com_html = fh.read().decode('utf-8')
+    parser.feed(monzo_com_html)
+    print(parser.temp_urls)
