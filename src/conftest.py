67261dce011d83acc1e49ec4fc04da7e8295287e
Le Quoc Viet <vietlq85@gmail.com>
2019-05-11 17:03:39 +0100

    added comments in conftest

---
+++ src/conftest.py
@@ -0,0 +1,4 @@
+# Required for hinting import path to pytest
+# https://docs.pytest.org/en/latest/pythonpath.html
+# https://stackoverflow.com/a/50610630/128028
+# https://docs.pytest.org/en/latest/goodpractices.html
