3bc7d8f657cf00fad6302d5e202c9f6f8a2a55d6
Le Quoc Viet <vietlq85@gmail.com>
2019-05-11 18:20:50 +0100

    added is_charset_supported, split_content_type is_content_type_valid

--- src/sitemapbuilder/urlhtmlparser.py
+++ src/sitemapbuilder/urlhtmlparser.py
@@ -1,15 +1,40 @@
 """Extract unique URLs from a given HTML content & hinted seed URL"""
 
+import re
 from html.parser import HTMLParser
 from urllib.parse import urljoin, urlparse
 
 ERR_STR_BAD_SCHEME = "Bad scheme. Expected http(s)"
+ERR_STR_UNSUPPORTED_CHARSET = \
+    "Unsupported charset. Accepting only US-ASCII and UTF-8"
+CONTENT_TYPE_RE_PAT = re.compile(r'^(text/html); charset=(.*)$')
 
 
 def is_scheme_http_https(url):
     """Return True of the scheme is either http or https"""
     result = urlparse(url.lower())
     return result.scheme in ['http', 'https']
+
+
+def is_charset_supported(charset):
+    """Check if the charset is supported"""
+    return charset.upper() in ['US-ASCII', 'UTF-8']
+
+
+def split_content_type(content_type_str):
+    """Split Content-Type string"""
+    result = CONTENT_TYPE_RE_PAT.match(content_type_str)
+    if result:
+        return 'text/html', result.groups()[1]
+    return None, None
+
+
+def is_content_type_valid(content_type_str):
+    """Validate Content-Type string"""
+    content_type, charset = split_content_type(content_type_str)
+    return content_type and charset \
+        and (content_type == 'text/html') \
+        and is_charset_supported(charset)
 
 
 class UrlHtmlParser(HTMLParser):
